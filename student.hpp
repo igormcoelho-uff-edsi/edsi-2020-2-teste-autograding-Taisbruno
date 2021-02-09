0c83a1cdeae1736cb6bfce6a2ac0d8efd9c8cda5
Taisbruno <78763729+Taisbruno@users.noreply.github.com>
2021-02-09 15:58:16 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -3,15 +3,11 @@
 #include <iostream> // print library
 
 // Exercise 1: implements function 'add', that adds two numbers
-int add(int a, int b); {
-   return a+b;
-}
+int add(int a, int b)
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
-int mul(int a, int b); {
-   return a*b;
-}
-
+int mul(int a, int b)
+   
 // ===============
 // implementations
 
